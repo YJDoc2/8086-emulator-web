5fc46309afb092609f8701e4e2b582e3a6224972
YatharthVyas <yatharth2000.vyas@gmail.com>
2021-04-16 18:36:36 +0530

    Add Tutorial, Update Toast location and Change last mod date in sitemap.xml

--- webapp/src/components/router.js
+++ webapp/src/components/router.js
@@ -1,60 +1,60 @@
-import React, { useState, useEffect } from 'react';
-import Home from '../pages/home';
-import Compiler from '../pages/compiler';
-import InstructionSet from '../pages/instructions';
-import Navbar from './navbar';
-import Footer from './footer';
+import React, { useState, useEffect } from "react";
+import Home from "../pages/home";
+import Compiler from "../pages/compiler";
+import InstructionSet from "../pages/instructions";
+import Navbar from "./navbar";
+import Footer from "./footer";
 import {
-  Route,
-  BrowserRouter as Router,
-  Switch,
-  Redirect,
-} from 'react-router-dom';
+	Route,
+	BrowserRouter as Router,
+	Switch,
+	Redirect,
+} from "react-router-dom";
 
 function RootRouter() {
-  const [wasm, setWasm] = useState(null);
-  useEffect(() => {
-    async function load() {
-      try {
-        const wasm = await import('emulator_8086');
-        setWasm(wasm);
-      } catch (err) {
-        console.error(
-          `Unexpected error in loadWasm. [Message: ${err.message}]`
-        );
-        setWasm(false);
-      }
-    }
-    load();
-  }, []);
-  const invalidRoute = () => <Redirect to='/8086-emulator-web/' />; //This will send user back to homepage
+	const [wasm, setWasm] = useState(null);
+	useEffect(() => {
+		async function load() {
+			try {
+				const wasm = await import("emulator_8086");
+				setWasm(wasm);
+			} catch (err) {
+				console.error(
+					`Unexpected error in loadWasm. [Message: ${err.message}]`
+				);
+				setWasm(false);
+			}
+		}
+		load();
+	}, []);
+	const invalidRoute = () => <Redirect to="/8086-emulator-web/" />; //This will send user back to homepage
 
-  return (
-    <Router>
-      <div id='page-container'>
-        <Navbar />
-        <div className='App'>
-          <Switch>
-            <Route exact path='/8086-emulator-web/' component={Home} />
-            <Route
-              exact
-              path='/8086-emulator-web/compile'
-              component={() => <Compiler wasm={wasm} />}
-            />
-            <Route
-              exact
-              path='/8086-emulator-web/help'
-              component={InstructionSet}
-            />
-            <Route component={invalidRoute} />
-          </Switch>
-        </div>
-      </div>
-      <footer id='footer'>
-        <Footer />
-      </footer>
-    </Router>
-  );
+	return (
+		<Router>
+			<div id="page-container">
+				<Navbar />
+				<div className="App">
+					<Switch>
+						<Route exact path="/8086-emulator-web/" component={Home} />
+						<Route
+							exact
+							path="/8086-emulator-web/compile"
+							component={() => <Compiler wasm={wasm} />}
+						/>
+						<Route
+							exact
+							path="/8086-emulator-web/help"
+							component={InstructionSet}
+						/>
+						<Route component={invalidRoute} />
+					</Switch>
+				</div>
+			</div>
+			<footer id="footer">
+				<Footer />
+			</footer>
+		</Router>
+	);
 }
 
 export default RootRouter;
